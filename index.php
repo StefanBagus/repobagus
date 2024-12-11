2b130fee007b42e5711535bac54e57dc8bfc373f
StefanoIW <gunturtjoa18@gmail.com>
2024-12-11 23:46:23 +0700

    menambah tulisan pada index.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 HALO HALO HALO
 
 saya berkuliah di unika
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GG GEMING</title>
+</head>
+<body>
+    
+</body>
+</html>
